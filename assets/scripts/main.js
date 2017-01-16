c50025f80135bb4ff59a6f9ea5c78bbda451f83a
Jimmie Gisclair <jimmiegisclair@gmail.com>
2017-01-16 01:42:04 -0800

    Code Cleanup/Refactor
    Cleaned up the main.js file to remove unnecessary complexity. Removed a
    new line in index.html

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,25 +1,23 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
 
-var map;
-var layer;
+window.onload = function() {
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+
+  var map;
+  var layer;
 
 
-var mainState = {
-    preload: function() {
-        game.load.tilemap('basic_map', 'assets/maps/basic_map.json', null, Phaser.Tilemap.TILED_JSON);
-        game.load.image('sprites', 'assets/maps/tmw_desert_spacing.png');
-    },
+  function preload() {
+    game.load.tilemap('basic_map', 'assets/maps/basic_map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('sprites', 'assets/maps/tmw_desert_spacing.png');
+  }
 
-    create: function() {
-        map = game.add.tilemap('basic_map');
-        map.addTilesetImage('tmw_desert_spacing', 'sprites');
-        layer = map.createLayer('Tile Layer 1');
-        layer.resizeWorld();
-    },
+  function create() {
+    map = game.add.tilemap('basic_map');
+    map.addTilesetImage('tmw_desert_spacing', 'sprites');
+    layer = map.createLayer('Tile Layer 1');
+    layer.resizeWorld();
+  }
 
-    update: function() {
-    }
+  function update() {
+  }
 };
-
-game.state.add('mainState', mainState);
-game.state.start('mainState');
